aa787b46e45a8e02288702aea40d4e756dea318e
arildo <arildohp@gmail.com>
2024-07-04 10:02:40 -0300

    criando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import Header from './components/Header'
 import { GlobalCss } from './styles'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/'
+  }
+])
 
 function App() {
   return (
---
+++ src/components/Header/Button/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { colors } from '../../../styles'
+
+export const ButtonContainer = styled.button`
+  background-color: ${colors.darkBlue};
+  color: ${colors.white};
+  border-radius: 5px;
+  align-items: center;
+  font-size: 22px;
+  font-weight: bold;
+`
+
+export const ButtonLink = styled(Link)`
+  background-color: ${colors.darkBlue};
+  color: ${colors.white};
+  border-radius: 5px;
+  align-items: center;
+  font-size: 22px;
+  font-weight: bold;
+`
